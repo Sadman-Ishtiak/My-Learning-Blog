24605beec7210e6ca0a2c2cf526c8345a63d6a61
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-08-04 11:54:02 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import os
+from datetime import datetime
 
 outoutJSON = ""
 FILE_LIST = []
+FILE_METADATA = {}
 STACK = []
 def read_dirs(dir):
     STACK.append(dir)
@@ -18,4 +20,4 @@ def read_dirs(dir):
 if __name__ == "__main__":
     read_dirs("./posts")
     for i in FILE_LIST:
-        print(i)
+        print(i, os.path.getsize(i), datetime.fromtimestamp(os.path.getmtime(i)))
